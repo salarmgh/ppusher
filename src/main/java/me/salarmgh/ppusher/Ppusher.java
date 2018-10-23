eecf3a1979f77dd53434faad155e5525259d9c29
Salar Moghaddam <salarmgh@gmail.com>
2018-10-23 17:02:37 +0330

    Bugfix

--- src/main/java/me/salarmgh/ppusher/Ppusher.java
+++ src/main/java/me/salarmgh/ppusher/Ppusher.java
@@ -6,47 +6,34 @@ import com.fasterxml.jackson.databind.ObjectReader;
 import okhttp3.*;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Ppusher {
-    public String getPusherURL() {
-        return pusherURL;
-    }
-
-    public String getMetric() {
-        return metric;
-    }
-
-    public String getProgram() {
-        return program;
-    }
-
-    public long getValue() {
-        return value;
-    }
-
-    private String pusherURL;
-    private String metric;
-    private String program;
-    private long value;
-
     private static final MediaType MEDIA_TYPE_X_FORM
             = MediaType.parse("application/x-www-form-urlencoded");
     private final OkHttpClient client = new OkHttpClient();
 
+    private String pusherURL;
+    private String metric;
+    private String program;
+
+    public String getPusherURL() { return pusherURL; }
+
+    public String getMetric() { return metric; }
+
+    public String getProgram() { return program; }
+
     public Ppusher(String pusherURL, String metric,
-                   String program, int value) {
+                   String program) {
         this.pusherURL = pusherURL;
         this.metric = metric;
         this.program = program;
-        this.value = value;
     }
 
-    public void sendMetrics() throws IOException {
-        String postBody = String.format("%s{name=\"%s\"} %d\n", this.metric, this.program, this.value);
+    public void sendMetrics(long value) throws IOException {
+        String postBody = String.format("%s{name=\"%s\"} %d\n", this.metric, this.program, value);
         Request request = new Request.Builder()
-                .url(String.format("%s/metrics/job/%s_%s/", this.pusherURL, this.metric, this.program))
+                .url(String.format("%s/metrics/job/%s_%s", this.pusherURL, this.metric, this.program))
                 .post(RequestBody.create(MEDIA_TYPE_X_FORM, postBody))
                 .build();
 
@@ -54,21 +41,24 @@ public class Ppusher {
         if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
     }
 
-    public List<Long> getMetrics() throws IOException {
+    public long getMetrics() throws IOException {
+        long value = 0;
         ObjectMapper om = new ObjectMapper();
-        List<Long> values = new ArrayList<Long>();
         Request request = new Request.Builder()
-                .url(String.format("%s/api/v1/query?query=%s/", this.pusherURL, this.metric))
+                .url(String.format("%s/api/v1/query?query=%s", this.pusherURL, this.metric))
                 .build();
         Response response = client.newCall(request).execute();
         if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
         JsonNode json = om.readTree(response.body().string());
-        for (JsonNode product : json) {
-            ObjectReader reader = om.readerFor(new TypeReference<List<Long>>() {
-            });
-            values = reader.readValue(product.get("value"));
+        for (JsonNode product : json.get("data").get("result")) {
+            if (this.program.equals(product.get("metric").get("name").asText())) {
+                ObjectReader reader = om.readerFor(new TypeReference<List<Long>>() {});
+                List<Long> values = reader.readValue(product.get("value"));
+                value = values.get(1);
+                break;
             }
-        return values;
+        }
+        return value;
     }
 }
